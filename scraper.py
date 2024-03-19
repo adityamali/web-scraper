2ed55b246741e2aae11eff75314c341fd08d3749
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2024-03-19 11:37:39 +0530

    intermediatary update 0.02

--- scraper.py
+++ scraper.py
@@ -4,22 +4,47 @@ import pandas as pd
 from selenium import webdriver
 
 browser = webdriver.ChromiumEdge()
-
 baseUrl = "https://www.bikedekho.com"
-brands = ["tvs", "bajaj", "royal-enfield", "yamaha", "honda", "hero", "suzuki", "ktm", "jawa", "harley-davidson", "ducati", "kawasaki", ]
-bikeUrls = []
-for brand in brands:
-    url = baseUrl + "/" + brand + "-bikes"
-    print(url)
+brands = ["tvs", "bajaj", "royal-enfield", "yamaha", "honda", "hero", "suzuki", "ktm", "jawa-motorcycles", "harley-davidson", "ducati", "kawasaki", ]
+bikeData = []
+
+
+# <----------Create Soup Function---------->
+def createSoup(url):
     browser.get(url)
-    # time.sleep(10)
     response = browser.page_source
-    # browser.quit()
     soup = BeautifulSoup(response, 'html.parser')
+    return soup
+
+
+# <----------Save to CSV Function---------->
+def saveToCSV(data):
+    df = pd.DataFrame(data, columns=['brand', 'url'])
+    df.to_csv('bike-data.csv', index=False)
+
+
+
+for brand in brands:
+    bikeListUrl = baseUrl + "/" + brand + "-bikes"
+    soup = createSoup(bikeListUrl)
     li = soup.select("ul.bikelist > li")
     for link in li:
         bike = link.find('a')
-        print(bike)
-        bikeUrls.append([bike])
+        if (bike == None):
+            continue
+        hrefList = bike.get_attribute_list("href")
+        for href1 in hrefList:
+            bikeLink = href1
+        print(bikeLink)
+        # bikeData.append([brand, bikeLink])
+
+        ### Into single bike page ###
+
+        singleBikePageURL = baseUrl + bikeLink
+        soup = createSoup(singleBikePageURL)
+
+
         # time.sleep(2)
     print("Brand: " + brand + "Done")
+
+saveToCSV(bikeData)
