e3493a4084e003ac14a65a933d3a6532eeed7d43
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2024-03-19 13:41:11 +0530

    Organisation of code & commenting

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ browser = webdriver.ChromiumEdge()
 baseUrl = "https://www.bikedekho.com"
 brands = ["tvs", "bajaj", "royal-enfield", "yamaha", "honda", "hero", "suzuki", "ktm", "jawa-motorcycles", "harley-davidson", "ducati", "kawasaki", ]
 bikeData = []
-
+dataDict = {}
 
 # <----------Create Soup Function---------->
 def createSoup(url):
@@ -16,35 +16,42 @@ def createSoup(url):
     soup = BeautifulSoup(response, 'html.parser')
     return soup
 
+# <----------Create Spec Dictionary Function---------->
+def saveToDict(dict, key, value):
+    dict[key] = value
+
+
 
 # <----------Save to CSV Function---------->
 def saveToCSV(data):
     df = pd.DataFrame(data, columns=['brand', 'url'])
     df.to_csv('bike-data.csv', index=False)
 
+# <----------GoTo Bikes Function---------->
+def getDetails(brands):
+    for brand in brands:
+        bikeListUrl = baseUrl + "/" + brand + "-bikes"
+        soup = createSoup(bikeListUrl)
+        li = soup.select("ul.bikelist > li")
+        for link in li:
+            bike = link.find('a')
+            if (bike == None):
+                continue
+            hrefList = bike.get_attribute_list("href")
+            for href1 in hrefList:
+                bikeLink = href1
+            print(bikeLink)
+            # bikeData.append([brand, bikeLink])
 
+            ### Into single bike page ###
 
-for brand in brands:
-    bikeListUrl = baseUrl + "/" + brand + "-bikes"
-    soup = createSoup(bikeListUrl)
-    li = soup.select("ul.bikelist > li")
-    for link in li:
-        bike = link.find('a')
-        if (bike == None):
-            continue
-        hrefList = bike.get_attribute_list("href")
-        for href1 in hrefList:
-            bikeLink = href1
-        print(bikeLink)
-        # bikeData.append([brand, bikeLink])
-
-        ### Into single bike page ###
-
-        singleBikePageURL = baseUrl + bikeLink
-        soup = createSoup(singleBikePageURL)
-
+            singleBikePageURL = baseUrl + bikeLink
+            soup = createSoup(singleBikePageURL)
 
         # time.sleep(2)
     print("Brand: " + brand + "Done")
 
+
+# <----------Master Code---------->
+getDetails(brands)
 saveToCSV(bikeData)
